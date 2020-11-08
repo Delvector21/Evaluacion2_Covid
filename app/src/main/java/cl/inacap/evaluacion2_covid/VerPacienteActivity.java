a53bd07592eff7297250a41485006fe7b2ba2167
Delvector21 <delvector@hotmail.com>
2020-11-07 22:39:31 -0300

    maqueta Registrar Paciente
    maquetacion del activity Registrar Paciente y ver paciente
    
    Co-Authored-By: barbarachester <69976270+barbarachester@users.noreply.github.com>

---
+++ app/src/main/java/cl/inacap/evaluacion2_covid/VerPacienteActivity.java
@@ -0,0 +1,14 @@
+package cl.inacap.evaluacion2_covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerPacienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_paciente);
+    }
+}
